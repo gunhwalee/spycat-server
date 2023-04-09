dee70e46c90ff0773fc71cdc8983c1fdbd8e0565
gunhwalee <ghlee2588@gmail.com>
2023-04-09 15:54:49 +0900

    feat: 서버추가 기능구현

--- src/routes/middlewares/serverHandler.js
+++ src/routes/middlewares/serverHandler.js
@@ -1,6 +1,16 @@
 const User = require("../../../models/User");
 const Server = require("../../../models/Server");
 
+const checkUrl = async url => {
+  try {
+    const server = await Server.findOne({ url });
+    if (server) return server;
+    return false;
+  } catch (err) {
+    throw Error(500);
+  }
+};
+
 exports.loadServerName = async (req, res, next) => {
   try {
     const user = await User.findById(req.user);
@@ -12,4 +22,33 @@ exports.loadServerName = async (req, res, next) => {
   } catch (err) {
     return next(err);
   }
+};
+
+exports.createServerInfo = async (req, res, next) => {
+  const userId = req.params.id;
+  const { name, url } = req.body;
+
+  try {
+    const hasUrl = await checkUrl(url);
+
+    if (hasUrl) {
+      return res.send({
+        result: "error",
+        message: "중복된 서버주소입니다.",
+      });
+    }
+
+    const server = await Server.create({ name, url });
+    const user = await User.findById(userId);
+    await User.findByIdAndUpdate(userId, {
+      servers: [...user.servers, server._id],
+    });
+  } catch (err) {
+    return next(err);
+  }
+
+  res.send({
+    result: "ok",
+    message: "서버가 정상적으로 추가됐습니다.",
+  });
 };
