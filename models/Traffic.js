0a64a8e81629ad816493ee58ee458aa0890b0926
gunhwalee <ghlee2588@gmail.com>
2023-04-12 15:32:27 +0900

    feat: 스키마 TTL 설정

--- models/Traffic.js
+++ models/Traffic.js
@@ -4,8 +4,19 @@ const trafficSchema = new mongoose.Schema(
   {
     path: { type: String, required: true, trim: true },
     host: { type: String, required: true, trim: true },
+    createdAt: { type: Date, expires: 2419200, default: Date.now },
   },
   { timestamps: true },
 );
 
-module.exports = mongoose.model("Traffic", trafficSchema);
+const Traffic = mongoose.model("Traffic", trafficSchema);
+const Server = mongoose.model("Server");
+
+Traffic.watch().on("change", async change => {
+  if (change.operationType === "delete") {
+    const { _id } = change.documentKey;
+    await Server.updateMany({ traffics: _id }, { $pull: { traffics: _id } });
+  }
+});
+
+module.exports = Traffic;
