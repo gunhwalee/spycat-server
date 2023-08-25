a079f23bb7dc9e5d62c3a8144764e6a7c4a157cb
gunhwalee <ghlee2588@gmail.com>
2023-08-25 18:26:09 +0900

    fix: cors whitelist 추가

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -3,13 +3,18 @@ import logger from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+const whiteList = ["https://spycat.fun", "http://localhost:3000"];
+
+const corsOptions = {
+  origin: (origin: string, callback: (err: Error, allow: boolean) => void) => {
+    if (whiteList.indexOf(origin) !== -1) callback(null, true);
+    else callback(new Error("Not Allowed Origin!"), false);
+  },
+  credentials: true,
+};
+
 export const expressLoader = async (app: Express) => {
-  app.use(
-    cors({
-      origin: "https://spycat.fun",
-      credentials: true,
-    }),
-  );
+  app.use(cors(corsOptions));
   app.use(cookieParser());
   app.use(logger("dev"));
   app.use(express.json());
